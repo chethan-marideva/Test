d0238730fa162c6cdacb1c97ec52f68c600f569c
Chethan M <chethan.m-dxc@hpe.com>
2023-05-04 23:50:17 +0530

    Inital Setup

---
+++ HelloWorld.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace HelloWorld.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
